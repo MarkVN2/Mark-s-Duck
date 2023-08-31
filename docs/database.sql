8788f23e0028ef09bb5b2a7608cf0d82d510e969
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2023-08-31 12:01:10 -0300

    [docs]:updated database .sql archive

--- docs/database.sql
+++ docs/database.sql
@@ -1,9 +1,11 @@
 create database duck;
 use duck;
+
 create table users (
-  `id_users` INT NOT NULL,
+  `id_users` INT auto_increment,
   `username` VARCHAR(45) NULL,
   `password` VARCHAR(45) NULL,
   `access_level` VARCHAR(45) NULL,
   PRIMARY KEY (`id_users`));
-select * from users
+  
+select * from users;
